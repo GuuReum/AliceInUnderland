00e92c660cbdaee3df0d545ba7d8b6ca7c908b66
User <rmsdndwo14@gmail.com>
2023-04-08 18:34:32 +0900

    임시 UI (방향키, 리로드 버튼, 탄환) 추가 버튼 클릭시 이벤트 추가

--- app/src/main/java/com/example/aliceinunderland/MainPlaying.java
+++ app/src/main/java/com/example/aliceinunderland/MainPlaying.java
@@ -2,21 +2,39 @@ package com.example.aliceinunderland;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.media.Image;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainPlaying extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        setContentView(R.layout.playing_main);
 
-    private long MaxPlayTime = 5000;//900000;
+        countdownText = findViewById(R.id.TimeText);
 
+        timer.start();
+    }
+
+
+
+
+
+
+
+
+
+    private long MaxPlayTime = 900000;
     private long temp_time;
-
     private TextView countdownText;
-
     CountDownTimer timer = new CountDownTimer(MaxPlayTime, 1000) {
 
         @Override
@@ -33,7 +51,6 @@ public class MainPlaying extends AppCompatActivity {
             finish();
         }
     };
-
     private void updateTimer() {
 
         int minute = (int) temp_time % 3600000 / 60000;
@@ -44,14 +61,90 @@ public class MainPlaying extends AppCompatActivity {
         countdownText.setText(Left_time);
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-        setContentView(R.layout.playing_main);
 
-        countdownText = findViewById(R.id.TimeText);
 
-        timer.start();
+
+
+
+
+
+
+    public void ClickLeftButton(View v) {
+        TextView moving = (TextView) findViewById(R.id.test);
+        moving.setText("Move to Left");
+    }
+    public void ClickRightButton(View v) {
+        TextView moving = (TextView) findViewById(R.id.test);
+        moving.setText("Move to Right");
+    }
+    int remainBullets = 5;
+    public void ClickReloadButton(View v) {
+        TextView moving = (TextView) findViewById(R.id.test);
+        moving.setText("Reload Magazine");
+
+        remainBullets = 5;
+
+        ImageView bullet1 = (ImageView)findViewById(R.id.bullet1);
+        ImageView bullet2 = (ImageView)findViewById(R.id.bullet2);
+        ImageView bullet3 = (ImageView)findViewById(R.id.bullet3);
+        ImageView bullet4 = (ImageView)findViewById(R.id.bullet4);
+        ImageView bullet5 = (ImageView)findViewById(R.id.bullet5);
+
+        bullet1.setVisibility(View.VISIBLE);
+        bullet2.setVisibility(View.VISIBLE);
+        bullet3.setVisibility(View.VISIBLE);
+        bullet4.setVisibility(View.VISIBLE);
+        bullet5.setVisibility(View.VISIBLE);
+    }
+    public void ClickShootButton(View v) {
+
+        TextView shooting = (TextView) findViewById(R.id.test);
+
+        switch (remainBullets){
+            case 0:
+                shooting.setText("Not Enough Bullets");
+                break;
+            case 1:
+                shooting.setText("Shot0");
+
+                ImageView bullet1 = (ImageView)findViewById(R.id.bullet1);
+                bullet1.setVisibility(View.INVISIBLE);
+                remainBullets--;
+
+                break;
+            case 2:
+                shooting.setText("Shot1");
+
+                ImageView bullet2 = (ImageView)findViewById(R.id.bullet2);
+                bullet2.setVisibility(View.INVISIBLE);
+                remainBullets--;
+
+                break;
+            case 3:
+                shooting.setText("Shot2");
+
+                ImageView bullet3 = (ImageView)findViewById(R.id.bullet3);
+                bullet3.setVisibility(View.INVISIBLE);
+                remainBullets--;
+
+                break;
+            case 4:
+                shooting.setText("Shot3");
+
+                ImageView bullet4 = (ImageView)findViewById(R.id.bullet4);
+                bullet4.setVisibility(View.INVISIBLE);
+                remainBullets--;
+
+                break;
+            case 5:
+                shooting.setText("Shot4");
+
+                ImageView bullet5 = (ImageView)findViewById(R.id.bullet5);
+                bullet5.setVisibility(View.INVISIBLE);
+                remainBullets--;
+
+                break;
+        }
+
     }
 }
