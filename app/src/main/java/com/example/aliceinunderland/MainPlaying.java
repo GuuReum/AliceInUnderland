92f33f91485fc2d7416db60440d44e3124c1f137
DESKTOP-HMSIGJP\GuReum <wognl766@gmail.com>
2023-04-28 19:02:08 +0900

    재장전 오류 해결

--- app/src/main/java/com/example/aliceinunderland/MainPlaying.java
+++ app/src/main/java/com/example/aliceinunderland/MainPlaying.java
@@ -22,8 +22,85 @@ public class MainPlaying extends AppCompatActivity {
         setContentView(R.layout.playing_main);
 
         countdownText = findViewById(R.id.TimeText);
-
         timer.start();
+    }
+
+    Player player = new Player();
+
+    //왼쪽으로 이동 버튼(구현필요)
+    public void ClickLeftButton(View v) {
+
+    }
+
+    //오른쪽으로 이동 버튼(구현필요)
+    public void ClickRightButton(View v) {
+
+    }
+
+    //재장전 버튼
+    public void ClickReloadButton(View v) {
+        //player class의 재장전
+        player.reloadBullet();
+
+        //액티비티 화면 총알 설정
+        ImageView[] bullet = new ImageView[5];
+        //TODO:김재휘_ 총알의 이미지 표시->bullet 이미지 하나만 가지고 관리할 수는 없을까?
+        bullet[0] = (ImageView) findViewById(R.id.bullet1);
+        bullet[1] = (ImageView) findViewById(R.id.bullet2);
+        bullet[2] = (ImageView) findViewById(R.id.bullet3);
+        bullet[3] = (ImageView) findViewById(R.id.bullet4);
+        bullet[4] = (ImageView) findViewById(R.id.bullet5);
+
+        for (int i = 0; i < bullet.length; i++)
+            bullet[i].setVisibility(View.VISIBLE);
+    }
+
+    //화면 터치 시 총알 발사 이벤트
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == 1) {
+            if (player.getBullet() == 0) {
+                return false;
+            }
+            switch (player.getBullet()) {
+                case 1:
+                    ImageView bullet1 = (ImageView) findViewById(R.id.bullet1);
+                    bullet1.setVisibility(View.INVISIBLE);
+
+                    break;
+                case 2:
+                    ImageView bullet2 = (ImageView) findViewById(R.id.bullet2);
+                    bullet2.setVisibility(View.INVISIBLE);
+
+                    break;
+                case 3:
+                    ImageView bullet3 = (ImageView) findViewById(R.id.bullet3);
+                    bullet3.setVisibility(View.INVISIBLE);
+
+                    break;
+                case 4:
+                    ImageView bullet4 = (ImageView) findViewById(R.id.bullet4);
+                    bullet4.setVisibility(View.INVISIBLE);
+
+                    break;
+                case 5:
+                    ImageView bullet5 = (ImageView) findViewById(R.id.bullet5);
+                    bullet5.setVisibility(View.INVISIBLE);
+
+                    break;
+                default:
+                    break;
+            }
+
+            player.shootBullet();
+
+            float x = event.getX();
+            float y = event.getY();
+
+
+        }
+
+        return true;
     }
 
     //타이머 구현 line:28~57
@@ -31,21 +108,21 @@ public class MainPlaying extends AppCompatActivity {
     private long temp_time;
     private TextView countdownText;
     CountDownTimer timer = new CountDownTimer(MaxPlayTime, 1000) {
-
         @Override
         public void onTick(long l) {
             temp_time = l;
             updateTimer();
         }
+
         @Override
         public void onFinish() {
-            Intent intent = new Intent(MainPlaying.this,MainEnd.class);
+            Intent intent = new Intent(MainPlaying.this, MainEnd.class);
             startActivity(intent);
 
             finish();
         }
     };
-    
+
     //남은 플레이 시간 표시
     private void updateTimer() {
 
@@ -55,114 +132,5 @@ public class MainPlaying extends AppCompatActivity {
         String Left_time = "" + minute + ":" + seconds;
 
         countdownText.setText(Left_time);
-    }
-
-    Player player = new Player();
-
-    //왼쪽으로 이동 버튼(구현필요)
-    public void ClickLeftButton(View v) {
-        TextView moving = (TextView) findViewById(R.id.test);
-        moving.setText("Move to Left");
-    }
-    
-    //오른쪽으로 이동 버튼(구현필요)
-    public void ClickRightButton(View v) {
-        TextView moving = (TextView) findViewById(R.id.test);
-        moving.setText("Move to Right");
-    }
-    
-    //재장전 버튼
-    public void ClickReloadButton(View v) {
-        TextView moving = (TextView) findViewById(R.id.test);
-        moving.setText("Reload Magazine");
-
-        //재장전
-        player.reloadBullet();
-
-        ImageView[] bullet = new ImageView[4];
-        //TODO:김재휘_ 총알의 이미지 표시->bullet 이미지 하나만 가지고 관리할 수는 없을까?
-        bullet[0] = (ImageView)findViewById(R.id.bullet1);
-        bullet[1] = (ImageView)findViewById(R.id.bullet2);
-        bullet[2] = (ImageView)findViewById(R.id.bullet3);
-        bullet[3] = (ImageView)findViewById(R.id.bullet4);
-        bullet[4] = (ImageView)findViewById(R.id.bullet5);
-
-        for(int i = 0; i < bullet.length; i++)
-            bullet[i].setVisibility(View.VISIBLE);
-    }
-
-    //화면 터치 시 총알 발사 이벤트
-    @Override
-    public boolean onTouchEvent(MotionEvent event){
-        switch(event.getAction()){
-            case MotionEvent.ACTION_DOWN:{
-
-                break;
-            }
-            case MotionEvent.ACTION_MOVE:{
-
-                break;
-            }
-            //손가락을 떼면 총알 발사
-            case MotionEvent.ACTION_UP:{
-                TextView shooting = (TextView) findViewById(R.id.test);
-
-                switch (player.getBullet()){
-                    case 0:
-                        //player have 0 bullet -> print need to reload
-                        shooting.setText("Not Enough Bullets");
-                        break;
-                    //player have bullet -> invisible one bullet & remove one bullet
-                    case 1:
-                        shooting.setText("Shot0");
-
-                        ImageView bullet1 = (ImageView)findViewById(R.id.bullet1);
-                        bullet1.setVisibility(View.INVISIBLE);
-                        player.shootBullet();
-
-                        break;
-                    case 2:
-                        shooting.setText("Shot1");
-
-                        ImageView bullet2 = (ImageView)findViewById(R.id.bullet2);
-                        bullet2.setVisibility(View.INVISIBLE);
-                        player.shootBullet();
-
-                        break;
-                    case 3:
-                        shooting.setText("Shot2");
-
-                        ImageView bullet3 = (ImageView)findViewById(R.id.bullet3);
-                        bullet3.setVisibility(View.INVISIBLE);
-                        player.shootBullet();
-
-                        break;
-                    case 4:
-                        shooting.setText("Shot3");
-
-                        ImageView bullet4 = (ImageView)findViewById(R.id.bullet4);
-                        bullet4.setVisibility(View.INVISIBLE);
-                        player.shootBullet();
-
-                        break;
-                    case 5:
-                        shooting.setText("Shot4");
-
-                        ImageView bullet5 = (ImageView)findViewById(R.id.bullet5);
-                        bullet5.setVisibility(View.INVISIBLE);
-                        player.shootBullet();
-
-                        break;
-                }
-                break;
-            }
-            case MotionEvent.ACTION_CANCEL:{
-
-                break;
-            }
-            default:
-                break;
-        }
-        return true;
     }
 }
--- app/src/main/java/com/example/aliceinunderland/Player.java
+++ app/src/main/java/com/example/aliceinunderland/Player.java
@@ -3,19 +3,34 @@ package com.example.aliceinunderland;
 //플레이어 클래스..
 public class Player {
     private int bullet = 5;
+    private Vector playerVector = new Vector();
+
+    public void setVector(int x1, int y1, int x2, int y2) {
+        playerVector.beginX = x1;
+        playerVector.beginY = y1;
+        playerVector.endX = x2;
+        playerVector.endY = y2;
+    }
 
     //총알 한 발 사격
-    public void shootBullet(){
+    public void shootBullet() {
         bullet--;
     }
 
     //총알 재장전
-    public void reloadBullet(){
+    public void reloadBullet() {
         bullet = 5;
     }
 
     //return 총알의 개수
-    public int getBullet(){
+    public int getBullet() {
         return bullet;
+    }
+
+    public boolean isDamaged(float x, float y) {
+        if ((playerVector.beginX <= x) && (x <= playerVector.beginY)) {
+
+        }
+        return true;
     }
 }
