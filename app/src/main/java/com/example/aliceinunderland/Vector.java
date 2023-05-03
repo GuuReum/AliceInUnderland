5aaeaefbfbb43da0ee06b7dfcc217c7e174bb469
DESKTOP-HMSIGJP\GuReum <wognl766@gmail.com>
2023-05-03 23:17:46 +0900

    메소드 추가

--- app/src/main/java/com/example/aliceinunderland/Vector.java
+++ app/src/main/java/com/example/aliceinunderland/Vector.java
@@ -3,4 +3,9 @@ package com.example.aliceinunderland;
 public class Vector {
     float vectorX;
     float vectorY;
+
+    public void setVector(float x, float y){
+        vectorX = x;
+        vectorY = y;
+    }
 }
