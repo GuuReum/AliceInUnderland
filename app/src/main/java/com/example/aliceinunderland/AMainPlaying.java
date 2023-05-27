50fb7755ff2924454192ca0730052f8da25ef264
User <rmsdndwo14@gmail.com>
2023-05-27 18:19:45 +0900

    shoot & reload 추가 (Enemy 사망 이벤트 아직 없음)

--- app/src/main/java/com/example/aliceinunderland/AMainPlaying.java
+++ app/src/main/java/com/example/aliceinunderland/AMainPlaying.java
@@ -2,6 +2,7 @@ package com.example.aliceinunderland;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
@@ -64,6 +65,91 @@ public class AMainPlaying extends AppCompatActivity {
     }
 
     public void onClickReloadBtn(View view) {
+        ImageView reloadBtn = findViewById(R.id.reloadBtn);
+        reloadBtn.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    if (player.isReloadAble()) {
+                        player.prepareReload();
+                        loadBulletImage();
+
+                        reloadloop:
+                        for (int i = 0; i < 6; i++) {
+
+                            new Handler().postDelayed(new Runnable() {  // 0.3초마다에 1탄 장전
+                                @Override
+                                public void run() {
+
+                                    if (player.insertBullet()) {
+                                        loadBulletImage(); //이미지
+                                    } else {
+                                        player.setReloadAble(true);
+                                        player.setShootAble(true);
+                                    }
+                                }
+                            }, 300 * i);
+
+                        }
+                        loadBulletImage();
+                    }
+                }
+                return true;
+            }
+        });
+    }
+
+    public void onTouchToShoot(View v) {
+        View shootview = findViewById(R.id.gameView);
+        shootview.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                float curX = event.getX();  //눌린 곳의 X좌표
+                float curY = event.getY();  //눌린 곳의 Y좌표
+
+                if (player.isShootAble()) {
+                    if (event.getAction() == MotionEvent.ACTION_UP) {
+                        if (player.getLoadedBullet() > 0) {
+                            player.shootBullet();
+                            loadBulletImage();
+                        }
+                    }
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    public void loadBulletImage() {
+
+        ImageView[] bullet = new ImageView[5];
+        bullet[0] = (ImageView) findViewById(R.id.bulletA1);
+        bullet[1] = (ImageView) findViewById(R.id.bulletA2);
+        bullet[2] = (ImageView) findViewById(R.id.bulletA3);
+        bullet[3] = (ImageView) findViewById(R.id.bulletA4);
+        bullet[4] = (ImageView) findViewById(R.id.bulletA5);
+
+        int i = player.getLoadedBullet();
+
+        switch (i) {
+            case 0:
+                //player have 0 bullet -> print need to reload
+                bullet[0].setImageResource(R.drawable.emptymag);
+                bullet[1].setImageResource(R.drawable.emptymag);
+                bullet[2].setImageResource(R.drawable.emptymag);
+                bullet[3].setImageResource(R.drawable.emptymag);
+                bullet[4].setImageResource(R.drawable.emptymag);
+                break;
+            case 5:
+                bullet[4].setImageResource(R.drawable.mag);
+                break;
+            //player have bullet -> invisible one bullet & remove one bullet
+            default:
+                bullet[i - 1].setImageResource(R.drawable.mag);
+                bullet[i].setImageResource(R.drawable.emptymag);
+                break;
+        }
 
     }
 
--- app/src/main/java/com/example/aliceinunderland/APlayer.java
+++ app/src/main/java/com/example/aliceinunderland/APlayer.java
@@ -2,6 +2,7 @@ package com.example.aliceinunderland;
 
 import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
+import android.os.Handler;
 
 public class APlayer {
     private int x = 0;
@@ -13,6 +14,22 @@ public class APlayer {
     }
     public void setAlive(boolean alive) {
         isAlive = alive;
+    }
+    private boolean isShootAble = true; // 사격 가능 여부
+    public boolean isShootAble() {return isShootAble;}
+    public void setShootAble(boolean shootable) {isShootAble = shootable;}
+
+    private boolean isReloadAble = true; // 재장전 가능 여부
+    public boolean isReloadAble() {return isReloadAble;}
+    public void setReloadAble(boolean reloadable) {isReloadAble = reloadable;}
+
+    private int loadedBullet = 5; //장전된 총알
+    public int getLoadedBullet() {
+        return loadedBullet;
+    }
+    private int remainBullet = 10;  //여분 총알
+    public int getRemainBullet() {
+        return remainBullet;
     }
 
     private Drawable playerImage;
@@ -48,5 +65,35 @@ public class APlayer {
 
     public int getSize() {
         return size;
+    }
+
+    public void shootBullet() {
+        --loadedBullet;
+        setShootAble(false);
+
+        new Handler().postDelayed(new Runnable() {  // 0.7초뒤에 사격 가능
+            @Override
+            public void run() {
+                setShootAble(true);
+            }
+        }, 700);}
+
+    public void prepareReload() {
+
+        setShootAble(false); //사격 불가
+        setReloadAble(false); //리로드 불가
+
+        //재장전 전 준비
+        remainBullet += loadedBullet;
+        loadedBullet = 0;
+    }
+
+    public boolean insertBullet() {
+        if ((remainBullet > 0) && (loadedBullet < 5)) {
+            loadedBullet++;
+            remainBullet--;
+            return true;
+        }
+        return false;
     }
 }
